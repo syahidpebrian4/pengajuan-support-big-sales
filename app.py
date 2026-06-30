cc8b1f0f2d7b4c9395e98bc226793de27fedf2d2
syahidpebrian4 <syahidpebrian4@gmail.com>
2026-06-30 09:35:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ if st.session_state.daftar_pengajuan:
     urutan_kolom = [
         "No Cust", "Nama", "Barcode", "Prodname", "QTY", "HK", 
         "Harga Cust", "No Pengajuan", "Gap", "Persen", "Potensi", 
-        "Support", "Rasio", "Keterangan" ]
+        "Support", "Rasio", "Keterangan"]
 
     df_tampil = df_tampil[urutan_kolom]
     st.table(df_tampil)
